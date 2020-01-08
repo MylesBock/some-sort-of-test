278fb7df9b740a0377bece54e9759731d80f6220
Myles Bock <Myles_Bock@comcast.com>
2020-01-08 10:46:51 -0700

    ruff kut but it works

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # some-sort-of-test
 the quick brown fox and such
+
+## getting started
+1. Create a venv
+2. install the requirements
+
---
+++ some_exam/problem_one.py
@@ -0,0 +1,20 @@
+import json
+"""
+problem one
+---
+Given some malformed json and a valid equivalent
+ write a function to transform the malformed json to the valid equivalent
+ 
+Requirements:
+only one import is allowed (json)
+all tests in `test/test_problem_one.py` must pass with pytest (see README.md)
+"""
+
+
+class ProblemOne:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def transform_me(some_json):
+        return False
